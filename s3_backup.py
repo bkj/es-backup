3e9775e2176fbbbe2f5c55c14206a7c2a12516b3
Ben Johnson <ben@gophronesis.com>
2015-12-10 14:03:30 -0500

    adding cli

--- s3_backup.py
+++ s3_backup.py
@@ -1,31 +1,48 @@
 # Installing plugin:
 # bin/plugin install elasticsearch/elasticsearch-cloud-aws/2.6.0
 
+import argparse
 import json
 from datetime import datetime
 import elasticsearch
 from elasticsearch import Elasticsearch
 from elasticsearch.client import SnapshotClient
 
+# --
+# CLI
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--create', dest = 'create', action="store_true")
+args = parser.parse_args()
+
+# --
+
 def timestamp():
 	return datetime.strftime(datetime.now(), '%Y%m%d%H%M%S')
 
-with open('config-private.json', 'rb') as f:
-	config = json.load(f)
+# --
+# Config
+
+config         = json.load(open('config.json'))
+config_private = json.load(open('config-private.json'))
+
+config['REPO_DEF']['settings']['access_key'] = config_private['access_key']
+config['REPO_DEF']['settings']['secret_key'] = config_private['secret_key']
+
+# --
+# Run
 
 client = Elasticsearch([{'host' : config['ES_HOST'], 'port' : config['ES_PORT']}], timeout = 20)
 sc     = SnapshotClient(client)
 
-# Can do this repeatedly
-# 
-# sc.create_repository(config['REPO_NAME'], config['REPO_DEF'])
+if args.create:
+    sc.create_repository(config['REPO_NAME'], config['REPO_DEF'])
 
-# Restrict to certain indices, if necessary
 body = {}
 if config['INDICES'] != '':
 	body['indices'] = config['INDICES']
 
-# Kick off backup creation
+
 print 'starting backup...'
 res = sc.create(
 	repository          = config['REPO_NAME'], 
